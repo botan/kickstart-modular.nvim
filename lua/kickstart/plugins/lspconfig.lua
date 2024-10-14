15509061617fd6d3e1b743029e2fb39f62eb515b
botan <botana@thousandeyes.com>
2024-10-14 11:16:13 +0100

    Add pyright and r_language_server

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -169,7 +169,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        r_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
