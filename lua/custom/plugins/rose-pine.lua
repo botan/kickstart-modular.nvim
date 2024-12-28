8f183b46c3c3096542b45b45c5790baaf16c4e14
botan <aginbotan@gmail.com>
2024-12-28 00:54:10 +0000

    Disable italic on rose-pine

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -2,6 +2,9 @@ return {
   'rose-pine/neovim',
   priority = 1000,
   init = function()
+    local rose_pine = require 'rose-pine'
+    rose_pine.setup { styles = { italic = false } }
+
     vim.cmd.colorscheme 'rose-pine'
     vim.cmd.hi 'Comment gui=none'
   end,
