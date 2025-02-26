bd9acef19950c5a4bea4e775f45cdb32c001db65
botan <botana@thousandeyes.com>
2025-02-26 18:41:53 +0000

    Set up Python, R, and Rust formatters

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -34,7 +34,9 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff_fix', 'ruff_format', 'ruff_orginize_imports' },
+        r = { 'air' },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
