ca1b10a6146abb1a0afb71be90ecfd3b6e56fb6d
botan <aginbotan@gmail.com>
2025-03-09 22:36:26 +0000

    Set up Bash LSP and formatter

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -37,6 +37,7 @@ return {
         python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
         r = { 'air' },
         rust = { 'rustfmt', lsp_format = 'fallback' },
+        -- sh = { 'shfmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
